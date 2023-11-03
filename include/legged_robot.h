79c89708d584e7fc094e7b7bd9b96c857f4c3bc3
brendanneal12 <125323287+brendanneal12@users.noreply.github.com>
2023-11-03 14:08:20 -0400

    Finish Legged Robot

--- include/legged_robot.h
+++ include/legged_robot.h
@@ -1,1 +1,60 @@
 #pragma once
+
+#include "mobile_robot.h"
+
+namespace RWA2
+{
+    class LeggedRobot final : public MobileRobot
+    {
+
+    private:
+        // ==================== attributes ====================
+        double height_;
+        int leg_strength_;
+        int number_of_legs_{2};
+
+        // ==================== methods ====================
+        void kick();
+        void jump(double amount);
+
+    protected:
+        // ==================== methods ====================
+        virtual void rotate(double angle) override;
+
+    public:
+        // ==================== constructor ====================
+        /**
+         * @brief Construct a new Wheeled Robot object
+         * - first argument: x position
+         * - second argument: y position
+         * - third argument: orientation
+         * - fourth argument: robot model name
+         * - fifth argument: battery model name
+         * - sixth argument: current battery charge
+         * - seventh argument: is charging?
+         * - eigth argument: height
+         * - ninth argument: leg strength
+         * - tenth argument: number of legs
+         */
+        LeggedRobot(double x, double y, double orientation, std::string botmodel, std::string battmodel,
+                    int current_charge, bool is_charging, double height, int leg_strength, int number_of_legs) : MobileRobot(x, y, orientation, botmodel, battmodel, current_charge, is_charging), 
+                    height_{height}, leg_strength_{leg_strength}, number_of_legs_{number_of_legs}
+        {
+        }
+
+        // ==================== methods ====================
+        /**
+         * @brief Move the aerial robot (override method)
+         *
+         * @param distance
+         * @param angle
+         */
+        virtual void move(double distance, double angle) override;
+
+        /**
+         * @brief Print the current status of the robot (override method)
+         */
+        virtual void print_status() override;
+
+    }; // class LeggedRobot
+} // namespace RWA2
---
+++ src/legged_robot.cpp
@@ -0,0 +1,57 @@
+#include "legged_robot.h"
+#include <chrono>
+#include <thread>
+
+void RWA2::LeggedRobot::kick()
+{
+    std::cout << model_ << "kicks with a leg strength of " << leg_strength_ << '\n';
+}
+
+void RWA2::LeggedRobot::jump(double amount)
+{
+    height_ = 0.1 * (leg_strength_ * amount);
+}
+
+void RWA2::LeggedRobot::rotate(double angle)
+{
+    std::cout << "LeggedRobot::" << model_ << " rotated" << angle << " degrees \n";
+}
+
+void RWA2::LeggedRobot::print_status()
+{
+    std::cout << "================\n";
+    std::cout << "LEGGED ROBOT\n";
+    std::cout
+        << "Position: (" << position_.first << ", "
+        << position_.second << "), Orientation: " << orientation_
+        << ", Speed: " << speed_ << "\n"
+        << ", Current Battery Charge: " << battery_.get_current_charge();
+    std::cout << "Height: " << height_ << " Leg Strength: " << leg_strength_ << " Number of Legs: " << number_of_legs_ << '\n';
+}
+
+void RWA2::LeggedRobot::move(double distance, double angle)
+{
+    if (distance > 10)
+    {
+        std::cout << "Legged Robot unable to move more than 10 m." << '\n';
+    }
+
+    if (battery_.get_current_charge() < 100 * height_)
+    {
+        std::cout << "Battery level is too low to jump " << height_ << " cm\n";
+        battery_.start_charging();
+    }
+
+    if (battery_.get_current_charge() < 1 * leg_strength_)
+    {
+        std::cout << "Battery level is too low to kick. \n";
+        battery_.start_charging();
+    }
+    LeggedRobot::get_sensor_reading(5);
+    LeggedRobot::rotate(angle);
+    LeggedRobot::jump(distance);
+    battery_.discharge(100 * distance);
+    LeggedRobot::kick();
+    battery_.discharge(1 * leg_strength_);
+    LeggedRobot::print_status();
+}
